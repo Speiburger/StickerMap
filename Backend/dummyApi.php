558bf3193bf05cfccd2b67d4371bd7e1645da9d0
Speiburger <justus.poepken@gmx.de>
2024-03-17 16:14:00 +0100

    Extended dummy API

--- Backend/dummyApi.php
+++ Backend/dummyApi.php
@@ -16,12 +16,21 @@ if ($apiMethod == 'getStickerList') {
 }';
 }
 
-if ($apiMethod == 'getStickerDetails') {
+if ($apiMethod == 'getStickerDetails' && $_POST['id'] == 0) {
 	echo '{
 "name": "test",
 "description": "Voluptatem sit placeat consequatur inventore sed. Ratione sit quo quae quasi reiciendis fuga. Quaerat dolorem vitae natus reiciendis beatae soluta sed non. In ullam non sed sunt sunt voluptatem qui aperiam. Possimus fugit qui mollitia earum perspiciatis",
 "lat": 0.351354,
 "long": 0.384975,
+"imageLink": "https://www.spd-friesoythe.de/wp-content/uploads/2021/07/1024-1536-1.jpg"
+}';
+}
+else if ($apiMethod == 'getStickerDetails' && $_POST['id'] == 1) {
+	echo '{
+"name": "Test2",
+"description": "Hallo, das hier ist ein Test",
+"lat": 0.12674,
+"long": 0.93456,
 "imageLink": "https://www.spd-friesoythe.de/wp-content/uploads/2021/07/1024-1536-1.jpg"
 }';
 }
